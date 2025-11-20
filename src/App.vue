7a310fdf77dd3ec2456273c0bfe73278dadc0356
Kaijeman <fachrudy.khalief@gmail.com>
2025-11-20 10:46:49 +0800

    initial commit

--- src/App.vue
+++ src/App.vue
@@ -9,6 +9,6 @@
 </template>
 
 <script setup>
-import Navbar from "./components/Navbar.vue";
-import Footer from "./components/Footer.vue";
+import Navbar from './components/Navbar.vue'
+import Footer from './components/Footer.vue'
 </script>
